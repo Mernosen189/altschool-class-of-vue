053eb1944885abda5e7fc8e8ecd2ce5a86e248c1
18-09-1980_sep <mernosen@gmail.com>
2023-02-07 23:17:55 +0100

    added the composable file

--- src/components/ColorPickerGame.vue
+++ src/components/ColorPickerGame.vue
@@ -10,25 +10,16 @@
 
 <script>
 import { ref, reactive } from "@vue/reactivity";
+import myPicker from "../composables/myPicker"
 
 export default {
   setup() {
-    const colors = ["green", "red", "blue", "purple"];
-    let message = ref("Pick a color...");
+    const colRef = ref("")
 
-    const matchColor = (value) => {
-      // do a random color based on the array index;
-      const randomNumber = Math.floor(Math.random() * 3) + 1; //between 1 - 4
+    const { colors, message, matchColor } = myPicker()
 
-      if (colors[randomNumber] === value) {
-        message.value = `You win... [answer: ${colors[randomNumber]}]`;
-        return;
-      }
-
-      message.value = `You loose [answer: ${colors[randomNumber]}]`;
-    };
-
-    return { colors, message, matchColor };
+    return { colors, message, matchColor }
+    
   },
 };
 </script>
